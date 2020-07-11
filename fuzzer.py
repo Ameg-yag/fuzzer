22b7a82d9f7d34f1298d1470e5c4a7fb9f425a65
Ryan McGrory <ryan.mcgrory@icloud.com>
2020-07-11 13:45:12 +1000

    add scaffold fuzzer file

---
+++ fuzzer.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from pwn import *
+
+# argument error checking
+    # 1 = binary name
+    # 2 = sampleinput
+
+
+PATH_TO_SANDBOX = "binaries/" # make empty string for deployment
+
+if (len(sys.argv) != 3):
+    sys.exit("Usage: python3 fuzzer.py [binaryName] [sampleInput]")
+
+binaryFileName = sys.argv[1]
+sampleInputFileName = sys.argv[2]
+
+
+if not (os.path.isfile(PATH_TO_SANDBOX + binaryFileName)):
+    sys.exit("Binary does not exist")
+
+if not (os.path.isfile(PATH_TO_SANDBOX + sampleInputFileName)):
+    sys.exit('Sample input does not exist')
+
+# open files
+
+
+# fuzz
+
+
+# print bad input (if any) [bad.txt]
+
+
+
