6317e84a184494e26da22dc110a1cdfbb30ddfd1
maleerose <rosegelliot@gmail.com>
2020-07-16 15:24:19 +1000

    csv1, csv2 working - csv tests empty and number of lines so far

--- fuzzer.py
+++ fuzzer.py
@@ -7,6 +7,127 @@ import csv
 import json
 import random 
 
+def read_csv(file):
+	csv_input = []
+	# Read and save csv output 
+	with open(inputFile) as file:
+		reader = csv.reader(file, delimiter=',')
+		for row in reader:
+			csv_input.append(row)
+	return csv_input
+
+def check_process(p,output):
+	p.proc.stdin.close()
+	if p.poll(block=True) < 0:
+		print("Found something..., saving to file bad.txt")
+		out = open("./bad.txt", "w")
+		out.writelines(output)
+		out.close()
+		exit()
+
+def empty_csv(binary):
+	p = process(binary)
+	p.send("")
+	check_process(p,"")
+
+def fields_csv(binary, csv_input):
+	p = process(binary)
+	error = []
+	p.sendline(','.join(csv_input[i]))
+	error.append( ','.join(csv_input[i]))
+	error.append('\n')
+	p.send('')
+	for string_length in range(1, 10000, 10):
+		for x in range(len(csv_input[1])):
+			out = ""
+			for i in range(0, string_length):
+				out += chr(random.randrange(0x41, 0x42))
+			csv_input[1][x] = out
+	
+		for r in range(1, len(csv_input)):
+			output = b""
+			output += ','.join(csv_input[r])
+			print(output)
+			p.sendline(output)
+			error.append(output)
+			error.append('\n')
+			if p.poll(block = False) != None:
+				break;
+
+		print("Testing... ", csv_input) 
+		if p.poll(block = False) != None:
+			break
+
+
+	p.proc.stdin.close()
+	if p.poll(block=True) < 0:
+		print("Found something..., saving to file bad.txt")
+		out = open("./bad.txt", "w")
+		out.writelines(error)
+		out.close()
+		exit()
+
+
+	p.close()
+
+	print("No vulnerabilties found")
+
+def fields_csv(binary, csv_input):
+	
+	p = process(binary)
+	error = []
+	p.sendline(','.join(csv_input[i]))
+	error.append( ','.join(csv_input[i]))
+	error.append('\n')
+	p.send('')
+	for string_length in range(1, 10000, 10):
+		for x in range(len(csv_input[1])):
+			out = ""
+			for i in range(0, string_length):
+				out += chr(random.randrange(0x41, 0x42))
+			csv_input[1][x] = out
+	
+		for r in range(1, len(csv_input)):
+			output = b""
+			output += ','.join(csv_input[r])
+			print(output)
+			p.sendline(output)
+			error.append(output)
+			error.append('\n')
+			if p.poll(block = False) != None:
+				break;
+
+		print("Testing... ", csv_input) 
+		if p.poll(block = False) != None:
+			break
+
+	check_process(p,output)
+	p.close()
+
+# Check if a enough CSV lines will crash the program 
+def lines_csv(binary, csv_input):
+	for length in range(0, 1000, 100):
+		p = process(binary)
+		error = []
+		for l in range(0,length):
+			if l < len(csv_input):
+				p.sendline(','.join(csv_input[l]))
+				error.append( ','.join(csv_input[l]) + '\n')
+			else:
+				p.sendline(','.join(csv_input[len(csv_input) - 1]))
+				error.append( ','.join(csv_input[len(csv_input) - 1]) + '\n')
+
+		check_process(p,error)
+		p.close()
+
+def csv_fuzzer(binary, inputFile):
+	csv_input = read_csv(sampleInputFileName)
+	# check nothing 
+	empty_csv(binary)
+	# check fields
+	lines_csv(binary, csv_input)
+
+
 # argument error checking
     # 1 = binary name
     # 2 = sampleinput
@@ -30,43 +151,9 @@ if not (os.path.isfile(inputFile)):
     sys.exit('Sample input does not exist')
 
 # open files
-# test input to determine file type
+# test input to determine input file type
 
-# save csv 
-csv_input = []
-
-# Read and save csv output 
-with open(inputFile) as file:
-	reader = csv.reader(file, delimiter=',')
-	i = 0
-	for row in reader:
-		csv_input.append(row)
-
-
-for x in range(0, 100):
-	for x in csv_input[1]:
-		string_length = random.randrange(0, 10001)
-		out = ""
-		for i in range(0, string_length):
-			out += chr(random.randrange(0, 255))
-		x = out
-
-	p = process(binary)
-	for r in csv_input:
-		p.send(','.join(r))
-	p.proc.stdin.close()
-	print("Testing... ", i) 
-
-	if p.poll(block=True) < 0:
-		print("Found something..., saving to file bas.txt")
-		out = open("./bad.txt", w)
-		out.writelines([output])
-		out.close()
-		exit()
-
-	p.close()
-
-print("No vulnerabilties found")
+csv_fuzzer(binary, inputFile)
 
 
 
