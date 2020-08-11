6d9d745b90ed2862c9e035402e1a7c7b2b98fe26
maleerose <rosegelliot@gmail.com>
2020-08-11 15:26:42 +1000

    fix typo

--- test.sh
+++ test.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 python3 py/fuzzer.py $1 $1.txt &
 wait
 sleep 1
 cat bad.txt | binaries/$1
-=======
-if [[ $# != 1 ]] || ! [[ "$1" =~ [a-zA-Z]+[0-9] ]]; then
-    echo "Usage: ./test.sh <binary-name>"
-    exit 1
-fi
 
-[[ -f bad.txt ]]  && rm bad.txt
-./py/fuzzer.py $1 $1.txt
-
-[[ -f bad.txt ]] && cat bad.txt | binaries/$1
-[[ -f core ]] && rm core
->>>>>>> XML
